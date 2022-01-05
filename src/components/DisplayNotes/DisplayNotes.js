9ecbeac37349b9b963ecb372becbfcf0c6c564a8
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-01-05 23:14:38 +0530

    added display notes

---
+++ src/components/DisplayNotes/DisplayNotes.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { NoteCard } from './NoteCard';
+
+export function DisplayNotes() {
+  function sortByTime(notesObject) {
+    const listOfIds = Object.keys(notesObject);
+    listOfIds.sort(
+      (a, b) => notesList[b].lastModified - notesList[a].lastModified
+    );
+    return listOfIds;
+  }
+
+  function sortByLabel(listOfIds) {
+    return listOfIds.filter((id) => notesList[id].label === selectedLabel);
+  }
+
+  function filterPinned(listOfIds) {
+    return listOfIds.filter((id) => notesList[id].pinned);
+  }
+
+  function filterUnpinned(listOfIds) {
+    return listOfIds.filter((id) => !notesList[id].pinned);
+  }
+
+  function displayNotes(listOfIds) {
+    return (
+      <div className="notes-flex-container">
+        {listOfIds.map((noteId) => (
+          <NoteCard noteId={noteId} />
+        ))}
+      </div>
+    );
+  }
+
+  function NotesCategories() {
+    const sortedByTime = sortByTime(notesList);
+    const filteredByLabel =
+      selectedLabel === '' ? sortedByTime : sortByLabel(sortedByTime);
+    const pinnedList = filterPinned(filteredByLabel);
+    const othersList = filterUnpinned(filteredByLabel);
+
+    return (
+      <div>
+        {pinnedList.length ? <h1 className="notes-pinned">Pinned</h1> : null}
+        {displayNotes(pinnedList)}
+        {othersList.length && pinnedList.length ? (
+          <h1 className="notes-others">Others</h1>
+        ) : null}
+        {displayNotes(othersList)}
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <NotesCategories />
+    </div>
+  );
+}
---
+++ src/components/DisplayNotes/NoteCard.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { NoteFooter } from '../Helpers/NoteFooter';
+import { UnpinSvg, PinSvg } from '../Helpers/Svg';
+import { useNotes } from '../../ContextProviders/NotesContext';
+
+export function NoteCard({ noteId }) {
+  const { notesList, setNotesList, setEditNote, setEditModal } = useNotes();
+  const note = notesList[noteId];
+
+  function displayEditModal() {
+    setEditModal(true);
+    setEditNote(note);
+  }
+
+  function togglePin() {
+    function getNewList(currentNotesList) {
+      const newNotesList = { ...currentNotesList };
+      delete newNotesList[noteId];
+      const newNote = { ...note };
+
+      if (note.pinned) {
+        newNote.pinned = false;
+      } else {
+        newNote.pinned = true;
+      }
+
+      return {
+        [noteId]: { ...newNote, lastModified: Date.now() },
+        ...newNotesList
+      };
+    }
+
+    setNotesList((currentNotesList) => getNewList(currentNotesList));
+  }
+
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: `${note.colour}`
+      }}
+      onClick={displayEditModal}
+      key={note.uuid}
+    >
+      <div className="card-title">{note.title}</div>
+
+      <button
+        className="card-pin-button"
+        onClick={(e) => {
+          togglePin();
+          e.stopPropagation();
+        }}
+      >
+        {note.pinned ? <UnpinSvg /> : <PinSvg />}
+      </button>
+
+      <p className="card-body">{note.body}</p>
+
+      <div className="card-footer">
+        <NoteFooter className="card-footer" note={note} />
+      </div>
+    </div>
+  );
+}
