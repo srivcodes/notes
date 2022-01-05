f37e5d1df64cef4695949b8595cc92174b205cd8
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-01-05 23:27:36 +0530

    making a new note

---
+++ src/components/EditNote/EditNote.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { NoteTitle } from '../Helpers/NoteTitle';
+import { NoteBody } from '../Helpers/NoteBody';
+import { NoteFooter } from '../Helpers/NoteFooter';
+import { useNotes } from '../../ContextProviders/NotesContext';
+
+export function EditNote() {
+  const { setNotesList, editNote, setEditNote, editModal, setEditModal } =
+    useNotes();
+
+  function saveNote() {
+    function getNewList(currentNotesList) {
+      const newNotesList = { ...currentNotesList };
+      delete newNotesList[editNote.uuid];
+      return { ...newNotesList, [editNote.uuid]: editNote };
+    }
+    setNotesList((currentNotesList) => getNewList(currentNotesList));
+  }
+
+  function closeModal() {
+    saveNote();
+    setEditModal(false);
+  }
+
+  return (
+    <div>
+      {editModal ? (
+        <div className="edit-component-container" onClick={closeModal}>
+          <div
+            className="note-taker"
+            onClick={(e) => e.stopPropagation()}
+            style={{ backgroundColor: `${editNote.colour}` }}
+          >
+            <NoteTitle note={editNote} setNote={setEditNote} />
+            <NoteBody note={editNote} setNote={setEditNote} />
+            <div className="footer">
+              <NoteFooter note={editNote} setNote={setEditNote} />
+            </div>
+            <button className="close" onClick={closeModal}>
+              Close
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+}
---
+++ src/components/NewNote/NewNote.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NoteTitle } from '../Helpers/NoteTitle';
+import { NoteBody } from '../Helpers/NoteBody';
+import { NoteFooter } from '../Helpers/NoteFooter';
+import { useNotes } from '../../ContextProviders/NotesContext';
+
+export function NewNote() {
+  const { note, setNote, addNote, newNoteFlag, setNewNoteFlag } = useNotes();
+
+  return (
+    <div
+      className="note-taker"
+      onClick={(e) => e.stopPropagation()}
+      style={{ backgroundColor: `${note.colour}` }}
+    >
+      <div style={{ display: newNoteFlag ? 'inline' : 'none' }}>
+        <NoteTitle note={note} setNote={setNote} />
+      </div>
+
+      <NoteBody note={note} setNote={setNote} setNewNoteFlag={setNewNoteFlag} />
+
+      <div
+        className="footer"
+        style={{ display: newNoteFlag ? 'inline' : 'none' }}
+      >
+        <NoteFooter note={note} setNote={setNote} />
+        <button className="close" onClick={addNote}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+}
