d1cc168eb6c3caabc2c94cf8035dbf6d9df0ff2f
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-01-05 15:36:06 +0530

    added colors

---
+++ src/components/Helpers/Colours.js
@@ -0,0 +1,15 @@
+export const colours = {
+    Default: "rgb(32, 33, 36)",
+    Red: "rgb(134, 30, 30, 0.4)",
+    Orange: "rgb(172, 96, 25, 0.4)",
+    Yellow: "rgb(190, 163, 29, 0.4)",
+    Green: "rgb(88, 141, 28, 0.4)",
+    Teal: "rgb(25, 117, 90, 0.4)",
+    Blue: "rgb(32, 44, 148, 0.4)",
+    Purple: "rgb(78, 33, 150, 0.4)",
+    Pink: "rgb(141, 39, 95, 0.4)",
+    Brown: "rgb(110, 51, 17, 0.4)",
+    Grey: "rgb(83, 82, 84, 0.4)",
+};
+
+//using rgb here as I can set opacity along with the colors!
