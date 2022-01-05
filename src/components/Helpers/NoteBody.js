6ec381672c655ac0c7bdf4b6bda3e07fd6058bc5
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-01-05 15:42:15 +0530

    making the body of the note

---
+++ src/components/Helpers/NoteBody.js
@@ -0,0 +1,41 @@
+import React, { useRef, useEffect } from 'react';
+
+export function NoteBody({ note, setNote, setNewNoteFlag }) {
+  let textAreaRef = useRef(null);
+
+  useEffect(() => {
+    if (textAreaRef) {
+      textAreaRef.style.height = '20px';
+      textAreaRef.style.height = textAreaRef.scrollHeight + 'px';
+    }
+  }, [note]);
+
+  function expandNewNote() {
+    if (setNewNoteFlag !== undefined) {
+      // i.e component is being used in New Note
+      setNewNoteFlag(true);
+    }
+  }
+
+  function saveNoteBody(e) {
+    expandNewNote();
+    setNote((currentNote) => ({
+      ...currentNote,
+      body: e.target.value,
+      lastEdited: Date.now()
+    }));
+  }
+
+  return (
+    <textarea
+      className="note-body-style"
+      placeholder="Take a note..."
+      type="text"
+      autoFocus
+      onClick={expandNewNote}
+      onChange={saveNoteBody}
+      ref={(ref) => (textAreaRef = ref)}
+      value={note.body}
+    />
+  );
+}
