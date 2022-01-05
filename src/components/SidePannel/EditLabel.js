5803f8c0a6b68d5dbac3f461004b37fe1c28c920
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-01-05 23:40:38 +0530

    added files for Editing the label

---
+++ src/components/SidePannel/EditLabel.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { CheckSvg } from '../Helpers/Svg';
+
+export function EditLabel({ label, setLabel, addNewLabel }) {
+  return (
+    <div onClick={(e) => e.stopPropagation()} className="edit-label-card">
+      <div className="edit-label-title">Add Label</div>
+
+      <input
+        className="edit-label-input"
+        type="text"
+        autoFocus
+        placeholder="New Label"
+        onChange={(e) => setLabel(e.target.value)}
+        value={label}
+      />
+      <button onClick={addNewLabel}>
+        <CheckSvg />
+      </button>
+    </div>
+  );
+}
