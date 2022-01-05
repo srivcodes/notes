761b59198ed3cfac18a861f78013714c25f93c41
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-01-05 23:08:12 +0530

    added features and enhancements

---
+++ src/components/Helpers/NoteFooterHelpers/ColourPicker.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ColourPalletSvg } from '../Svg';
+import { colours } from '../Colours';
+
+export function ColourPicker({ changeProperty }) {
+  function changeColour(e) {
+    changeProperty({
+      value: e.target.style.backgroundColor,
+      property: 'colour'
+    });
+  }
+
+  return (
+    <div className="colour-container">
+      <span className="colour-link">
+        <ColourPalletSvg />
+      </span>
+
+      <div className="colour-options">
+        {Object.keys(colours).map((colour) => (
+          <div
+            className="colour"
+            onClick={(e) => changeColour(e)}
+            style={{ backgroundColor: `${colours[colour]}` }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Helpers/NoteFooterHelpers/LabelSelector.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useLabel } from '../../../ContextProviders/LabelContext';
+
+export function LabelSelector({ note, changeProperty }) {
+  const { labelList } = useLabel();
+  function selectLabel({ e, label }) {
+    if (label === 'None') {
+      changeProperty({ value: '', property: 'label' });
+    } else {
+      changeProperty({
+        value: e.target.textContent,
+        property: 'label'
+      });
+    }
+  }
+
+  return (
+    <div className="label-container">
+      <span className="label-link label-selector">
+        {note.label === 'None' || note.label === '' ? 'Add Label' : note.label}
+      </span>
+
+      <div className="label-options">
+        {labelList.map((label) => (
+          <div
+            className="label-select"
+            onClick={(e) => selectLabel({ e, label })}
+            key={label}
+          >
+            {label}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
