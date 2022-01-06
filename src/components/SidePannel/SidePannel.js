12d3c78213235fc196cafbb238cff41700ac4050
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-01-06 20:40:01 +0530

    making the side pannel

---
+++ src/components/SidePannel/SidePannel.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { LabelSvg, HomeSvg, EditSvg } from '../Helpers/Svg';
+import { EditLabel } from './EditLabel';
+import { useLabel } from '../../ContextProviders/LabelContext';
+
+export function SidePannel() {
+  const { labelList, setLabelList, selectedLabel, setSelectedLabel } =
+    useLabel();
+  const [editLabelModal, setEditLabelModal] = useState(false);
+  const [label, setLabel] = useState('');
+
+  function LableList() {
+    return labelList.map((label) =>
+      label === 'None' ? null : (
+        <div
+          className={`label ${label === selectedLabel ? 'label-active' : null}`}
+          onClick={() => setSelectedLabel(label)}
+        >
+          <LabelSvg />
+          {label}
+        </div>
+      )
+    );
+  }
+
+  function addNewLabel() {
+    if (label !== '') {
+      setLabelList((currentLabels) => [...currentLabels, label]);
+      setLabel('');
+    }
+    setEditLabelModal(false);
+  }
+
+  return (
+    <div className="side-bar">
+      {editLabelModal ? (
+        <div className="edit-label-component-container" onClick={addNewLabel}>
+          <EditLabel
+            label={label}
+            setLabel={setLabel}
+            addNewLabel={addNewLabel}
+          />
+        </div>
+      ) : null}
+
+      <div
+        onClick={() => setSelectedLabel(label)}
+        className={`label home-label ${
+          selectedLabel === '' ? 'label-active' : null
+        }`}
+      >
+        <HomeSvg />
+        Notes
+      </div>
+
+      <LableList />
+
+      <div className="label" onClick={() => setEditLabelModal(true)}>
+        <EditSvg />
+        Add Label
+      </div>
+    </div>
+  );
+}
