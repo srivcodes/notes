1a25226e1ad08976ee986ea75921e9c61529f332
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-01-06 21:47:50 +0530

    made alterations in app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import "./index.css";
-import { NewNote } from "./components/NewNote/NewNote";
-import { SidePannel } from "./components/SidePannel/SidePannel";
-import { DisplayNotes } from "./components/DisplayNotes/DisplayNotes";
-import { EditNote } from "./components/EditNote/EditNote";
-import { useNotes } from "./ContextProviders/NotesContext";
-import { Header } from "./components/Header/Header";
+import './index.css';
+import { NewNote } from './components/NewNote/NewNote';
+import { SidePannel } from './components/SidePannel/SidePannel';
+import { DisplayNotes } from './components/DisplayNotes/DisplayNotes';
+import { EditNote } from './components/EditNote/EditNote';
+import { useNotes } from './ContextProviders/NotesContext';
+import { Header } from './components/Header/Header';
 
 export function App() {
-    const { addNote, newNoteFlag } = useNotes();
-    function saveOnOutOfFocus() {
-        if (newNoteFlag) {
-            addNote();
-        }
+  const { addNote, newNoteFlag } = useNotes();
+  function saveOnOutOfFocus() {
+    if (newNoteFlag) {
+      addNote();
     }
+  }
 
-    return (
-        <div className="App" onClick={saveOnOutOfFocus}>
-            <SidePannel />
-            <Header />
-            <div className="main-container">
-                <NewNote />
-                <DisplayNotes />
-                <EditNote />
-            </div>
-        </div>
-    );
+  return (
+    <div className="App" onClick={saveOnOutOfFocus}>
+      <SidePannel />
+      <Header />
+      <div className="main-container">
+        <NewNote />
+        <DisplayNotes />
+        <EditNote />
+      </div>
+    </div>
+  );
 }
