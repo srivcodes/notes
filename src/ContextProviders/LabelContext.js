81e40e48db3f2f404d265d1550d2728b070ac4b2
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-01-06 20:42:03 +0530

    made label context

---
+++ src/ContextProviders/LabelContext.js
@@ -0,0 +1,23 @@
+import { useContext, createContext, useState } from 'react';
+const LabelContext = createContext();
+
+export function LabelProvider({ children }) {
+  const [labelList, setLabelList] = useState(['None', 'Important', 'Todo']);
+  const [selectedLabel, setSelectedLabel] = useState('');
+  return (
+    <LabelContext.Provider
+      value={{
+        labelList,
+        setLabelList,
+        selectedLabel,
+        setSelectedLabel
+      }}
+    >
+      {children}
+    </LabelContext.Provider>
+  );
+}
+
+export function useLabel() {
+  return useContext(LabelContext);
+}
