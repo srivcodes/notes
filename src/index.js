a0a11acfce89e7c86b636dde3a1ee656fb99c83e
SriV-dya <74835693+SriV-dya@users.noreply.github.com>
2022-01-06 21:41:20 +0530

    fixes in index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
-import { NotesProvider } from "./ContextProviders/NotesContext";
-import { LabelProvider } from "./ContextProviders/LabelContext";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import { NotesProvider } from './ContextProviders/NotesContext';
+import { LabelProvider } from './ContextProviders/LabelContext';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <NotesProvider>
-            <LabelProvider>
-                <App />
-            </LabelProvider>
-        </NotesProvider>
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <NotesProvider>
+      <LabelProvider>
+        <App />
+      </LabelProvider>
+    </NotesProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
